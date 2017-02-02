2c3ce1face54f8f3582770d793aefc272fdf4375
Mike Hurl <mikehurl@gmail.com>
2017-02-02 11:37:01 +0000

    restarts the rails & react project to fix routing issues

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,8 +1,10 @@
-class WineController < ApplicationController
+class WinesController < ApplicationController
 
   def index
     @wines = Wine.all
     render component: 'Wines', props: { wines: @wines }
   end
+
+
 
 end
