06bbea5c956199b7ae8c6314c69b8fe47f7cacd5
Oscar Barlow <oscar.barlow@gmail.com>
2017-02-02 14:47:47 +0000

    food initialises with arguments and dependency injection to create pairing class

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,4 +3,8 @@ class Food < ApplicationRecord
   validates :sweet, :name, presence: true
   validates :sweet, inclusion: [0..5]
 
+  def initialize(args, pairing_klass)
+    pairing_klass.new(self)
+  end
+
 end
