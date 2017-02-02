7462db46d3ba0ee43946a812864f15c5269345f2
Mike Hurl <mikehurl@gmail.com>
2017-02-02 11:47:01 +0000

    adds latest csv file containing wine info

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,10 +1,19 @@
 class WinesController < ApplicationController
 
   def index
-    @wines = Wine.all
-    render component: 'Wines', props: { wines: @wines }
+    # @wines = Wine.all
+    # render component: 'Wines', props: { wines: @wines }
   end
 
+  def show
+    @wine = Wine.find_by(name: wine_params[:q])
+    erb :'show/:id'
+  end
 
+  private
+
+  def wine_params
+    params.permit(:q)
+  end
 
 end
