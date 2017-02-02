bcee99c28578a4e3924a4e6d0c782ffa75cfec63
Oscar Barlow <oscar.barlow@gmail.com>
2017-02-02 15:44:32 +0000

    creating a food through the new form creates a new Food object

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,22 @@
 class FoodsController < ApplicationController
 
+  def index
+    @foods = Food.all
+  end
+
   def new
     @food = Food.new()
+  end
+
+  def create
+    @food = Food.create(food_params)
+    redirect_to foods_path
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :sweet)
   end
 
 end
