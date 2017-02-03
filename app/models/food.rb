cf6872129ab15d8f352ac9d24997a388bc437938
Mike Hurl <mikehurl@gmail.com>
2017-02-03 13:14:00 +0000

    validates the values of umami to be between zero and five

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,7 +3,7 @@ class Food < ApplicationRecord
   belongs_to :pairing
 
   validates :sweet, :umami, :name, presence: true
-  validates_numericality_of :sweet, :greater_than_or_equal_to => 0
-  validates_numericality_of :sweet, :less_than_or_equal_to => 5
+  validates_numericality_of :sweet, :umami, :greater_than_or_equal_to => 0
+  validates_numericality_of :sweet, :umami, :less_than_or_equal_to => 5
 
 end
--- spec/food_spec.rb
+++ spec/food_spec.rb
@@ -6,28 +6,42 @@ describe Food do
     @pairing = Pairing.create
   end
 
-  it "should throw an error if you do not give it a sweet value on initialization" do
-    expect{ Food.create({pairing_id:@pairing.id}) }.not_to change{Food.count}
-  end
+  it {should belong_to :pairing }
 
   it "should throw an error if you do not give it a name on initialization" do
     expect{ Food.create({sweet: 3, pairing_id:@pairing.id}) }.not_to change{Food.count}
   end
 
-  it "should throw an error if you give it a sweet value of less than zero" do
-    expect{ Food.create({name:"Sweeeeeeet", sweet: -1, pairing_id:@pairing.id}) }.not_to change{Food.count}
+  describe '#sweet' do
+    it "should throw an error if you do not give it a sweet value on initialization" do
+      expect{ Food.create({pairing_id:@pairing.id}) }.not_to change{Food.count}
+    end
+
+    it "should throw an error if you give it a sweet value of less than zero" do
+      expect{ Food.create({name:"Sweeeeeeet", sweet: -1, pairing_id:@pairing.id}) }.not_to change{Food.count}
+    end
+
+    it "should throw an error if you give it a sweet value of greater than five" do
+      food = Food.create(name:"Sweeeeeeet", sweet:6, pairing_id:@pairing.id)
+      expect{ food.save }.not_to change{Food.count}
+    end
+
   end
 
-  it "should throw an error if you give it a sweet value of greater than five" do
-    food = Food.create(name:"Sweeeeeeet", sweet:6, pairing_id:@pairing.id)
-    expect{ food.save }.not_to change{Food.count}
-  end
+  describe '#umami' do
+    it "should throw an error if you do not give it an umami value on initialization" do
+      food = Food.new(name:"Umami", sweet:1, pairing_id:@pairing.id)
+      expect{ food.save }.not_to change{Food.count}
+    end
 
-  it {should belong_to :pairing }
+    it "should throw an error if you give it an umami value of less than zero" do
+      expect{ Food.create({name:"Umami", sweet:2, umami:-1, pairing_id:@pairing.id}) }.not_to change{Food.count}
+    end
 
-  it "should throw an error if you do not give it an umami value on initialization" do
-    food = Food.new(name:"Umami", sweet:1, pairing_id:@pairing.id)
-    expect{ food.save }.not_to change{Food.count}
+    it "should throw an error if you give it an umami value of greater than five" do
+      food = Food.new(name:"Umami", sweet:2, umami:7, pairing_id:@pairing.id)
+      expect{ food.save }.not_to change{Food.count}
+    end
 
   end
 
