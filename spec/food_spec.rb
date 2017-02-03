d28b25754167dc202f82773907d813b7b62a36e8
malin patel <malin.patel@icloud.com>
2017-02-03 13:23:54 +0000

    Pairing tests commented out

--- spec/food_spec.rb
+++ spec/food_spec.rb
@@ -7,23 +7,23 @@ describe Food do
     expect(Food.new(args)).to be_an_instance_of(Food)
   end
 
-  # let!(:pairing) { spy("pairing") }
-  #
-  # it "should throw an error if you do not give it a sweet value on initialization" do
-  #   expect{ Food.create({}) }.not_to change{Food.count}
-  # end
-  #
-  # it "should throw an error if you do not give it a name on initialization" do
-  #   expect{ Food.create({sweet: 3}) }.not_to change{Food.count}
-  # end
-  #
-  # it "should throw an error if you give it a sweet value of less than zero" do
-  #   expect{ Food.create({name:"Sweeeeeeet", sweet: -1}) }.not_to change{Food.count}
-  # end
-  #
-  # it "should throw an error if you give it a sweet value of greater than five" do
-  #   expect{ Food.create({name:"Sweeeeeeet", sweet:6}) }.not_to change{Food.count}
-  # end
+  let!(:pairing) { spy("pairing") }
+
+  it "should throw an error if you do not give it a sweet value on initialization" do
+    expect{ Food.create({}) }.not_to change{Food.count}
+  end
+
+  it "should throw an error if you do not give it a name on initialization" do
+    expect{ Food.create({sweet: 3}) }.not_to change{Food.count}
+  end
+
+  it "should throw an error if you give it a sweet value of less than zero" do
+    expect{ Food.create({name:"Sweeeeeeet", sweet: -1}) }.not_to change{Food.count}
+  end
+
+  it "should throw an error if you give it a sweet value of greater than five" do
+    expect{ Food.create({name:"Sweeeeeeet", sweet:6}) }.not_to change{Food.count}
+  end
 
   describe 'checking the type of flavour' do
 
