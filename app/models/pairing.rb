8504d62cfafe56c852ac8cc359afc5797c03cb79
Oscar Barlow <oscar.barlow@gmail.com>
2017-02-06 17:44:39 +0000

    move matches constant hash to be private, in pairing class

--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -4,23 +4,9 @@ class Pairing < ApplicationRecord
 
   has_one :food
 
-  MATCHES = {
-    sweet: 'sweet >= 5',
-    spicy: 'sweet >= 2.6 and sweet <= 5 and fruity >= 5.1 and acid >= 5.1 and alcohol <= 5',
-    sour: 'sweet >= 5 and bitter <= 2.5 and oaky <= 2.5',
-    umami: 'sweet >= 5 and bitter <= 2.5 and fruity >= 5',
-    bitter: 'bitter <= 2.5 and oaky <= 2.5',
-    umami_x_sweet: 'fruity >= 5 and acid >= 2.6 and acid <= 5',
-    umami_x_sour: 'oaky <= 5 and acid >= 7.5',
-    umami_x_bitter: 'bitter <= 2.5 and oaky <= 2.5',
-  }
 
   def get_wine(wine_klass)
     @wine = wine_klass.where(MATCHES[food.flavour])
-  end
-
-  def get_umami_x_bitter_wine(wine_klass)
-    @wine = wine_klass.where('bitter <= 2.5 and oaky <= 2.5')
   end
 
   def get_sweet_x_sour_wine(wine_klass)
@@ -42,4 +28,18 @@ class Pairing < ApplicationRecord
   def get_sour_x_spicy_wine(wine_klass)
     @wine = wine_klass.where('sweet >= 5 and bitter <= 2.5 and alcohol <= 5')
   end
+
+  private
+
+    MATCHES = {
+      sweet: 'sweet >= 5',
+      spicy: 'sweet >= 2.6 and sweet <= 5 and fruity >= 5.1 and acid >= 5.1 and alcohol <= 5',
+      sour: 'sweet >= 5 and bitter <= 2.5 and oaky <= 2.5',
+      umami: 'sweet >= 5 and bitter <= 2.5 and fruity >= 5',
+      bitter: 'bitter <= 2.5 and oaky <= 2.5',
+      umami_x_sweet: 'fruity >= 5 and acid >= 2.6 and acid <= 5',
+      umami_x_sour: 'oaky <= 5 and acid >= 7.5',
+      umami_x_bitter: 'bitter <= 2.5 and oaky <= 2.5',
+    }
+    
 end
