1d98b0ec4d4c478d828bbb449a40e5f8ed6bba20
Oscar Barlow <oscar.barlow@gmail.com>
2017-02-06 17:49:57 +0000

    pairing can get wine for sour and bitter food

--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -9,10 +9,6 @@ class Pairing < ApplicationRecord
     @wine = wine_klass.where(MATCHES[food.flavour])
   end
 
-  def get_sweet_x_spicy_wine(wine_klass)
-    @wine = wine_klass.where('sweet >= 5 and herby <= 2.5 and fruity >= 4.5 and acid >= 7.5')
-  end
-
   def get_sour_x_bitter_wine(wine_klass)
     @wine = wine_klass.where('oaky <= 2.5 and bitter <= 2.5 and acid >= 5')
   end
@@ -35,6 +31,7 @@ class Pairing < ApplicationRecord
       sweet_x_sour: 'sweet >= 5 and acid >= 7.5',
       sweet_x_bitter: 'sweet >= 5 and bitter <= 2.5 and fruity >= 4 and acid >= 7.5',
       sweet_x_spicy: 'sweet >= 5 and herby <= 2.5 and fruity >= 4.5 and acid >= 7.5',
+      sour_x_bitter: 'oaky <= 2.5 and bitter <= 2.5 and acid >= 5',
     }
 
 end
--- spec/pairing_spec.rb
+++ spec/pairing_spec.rb
@@ -101,6 +101,13 @@ describe Pairing do
       expect(wine_klass).to have_received(:where).with(criteria)
     end
 
+    it 'should be able to get wine appropriate for sour and bitter food' do
+      allow(pairing).to receive(:food).and_return(sour_x_bitter_food)
+      pairing.get_wine(wine_klass)
+      criteria = 'oaky <= 2.5 and bitter <= 2.5 and acid >= 5'
+      expect(wine_klass).to have_received(:where).with(criteria)
+    end
+
   end
 
 end
