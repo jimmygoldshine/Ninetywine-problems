ae555f20c470e9410496b4d14a73b553b3e927dc
Oscar Barlow <oscar.barlow@gmail.com>
2017-02-07 15:39:13 +0000

    refactor jQuery function for fading in form sliders on new food view

--- app/assets/javascripts/food.js
+++ app/assets/javascripts/food.js
@@ -5,14 +5,13 @@ $(document).ready(function() {
     $(element).hide();
   });
 
-  var i = 0
+  var i = 0;
   $("#next-button").click(function() {
-      $(foodCharacteristics[i]).show( "fade" );
-      if (foodCharacteristics[i] == "#bitter") {
+    $(foodCharacteristics[i]).show( "fade" );
+    i++;
+    if (foodCharacteristics[i] == undefined) {
       $(this).hide();
       $("#submit-button").show( "fade" );
-    } else {
-      $(foodCharacteristics[i]).show( "fade" );
-      i++;
     };
   });
+});
