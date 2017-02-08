e498126fa616ba08d8135de25d29af845454c5fb
Mike Hurl <mikehurl@gmail.com>
2017-02-08 13:38:35 +0000

    refactors #get_wine methid so that is returns wine based on the sweetness level of the food, so it no longer matters that a sweet food has to be over 3.5 to be matched to sweet wine and instead takes into account the sweet value of the food and applies an alogorithm that returns a wine within a range of sweetness

--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -6,7 +6,14 @@ class Pairing < ApplicationRecord
 
 
   def get_wine(wine_klass)
-    @wine = wine_klass.where(MATCHES[food.flavour]).limit(3)
+    if strongest_flavour == :sweet
+      food_flavours[:sweet] * 2
+      lower = (food_flavours[:sweet] * 2) - 0.5
+      upper = (food_flavours[:sweet] * 2) + 0.5
+      @wine = wine_klass.where("sweet >= #{lower} and sweet <= #{upper}")
+    else
+      @wine = wine_klass.where(MATCHES[food.flavour]).limit(3)
+    end
   end
 
   def strongest_flavour
@@ -23,20 +30,20 @@ class Pairing < ApplicationRecord
 
   private
 
-    MATCHES = {
-      sweet: 'sweet >= 5',
-      spicy: 'sweet >= 2.6 and sweet <= 5 and fruity >= 5.1 and acid >= 5.1 and alcohol <= 5',
-      sour: 'sweet >= 5 and bitter <= 2.5 and oaky <= 2.5',
-      umami: 'sweet >= 5 and bitter <= 2.5 and fruity >= 5',
-      bitter: 'bitter <= 2.5 and oaky <= 2.5',
-      umami_x_sweet: 'fruity >= 5 and acid >= 2.6 and acid <= 5',
-      umami_x_sour: 'oaky <= 5 and acid >= 7.5',
-      umami_x_bitter: 'bitter <= 2.5 and oaky <= 2.5',
-      sweet_x_sour: 'sweet >= 5 and acid >= 7.5',
-      sweet_x_bitter: 'sweet >= 5 and bitter <= 2.5 and fruity >= 4 and acid >= 7.5',
-      sweet_x_spicy: 'sweet >= 5 and herby <= 2.5 and fruity >= 4.5 and acid >= 7.5',
-      sour_x_bitter: 'oaky <= 2.5 and bitter <= 2.5 and acid >= 5',
-      sour_x_spicy: 'sweet >= 5 and bitter <= 2.5 and alcohol <= 5',
-    }
+  MATCHES = {
+    sweet: 'sweet >= 5',
+    spicy: 'sweet >= 2.6 and sweet <= 5 and fruity >= 5.1 and acid >= 5.1 and alcohol <= 5',
+    sour: 'sweet >= 5 and bitter <= 2.5 and oaky <= 2.5',
+    umami: 'sweet >= 5 and bitter <= 2.5 and fruity >= 5',
+    bitter: 'bitter <= 2.5 and oaky <= 2.5',
+    umami_x_sweet: 'fruity >= 5 and acid >= 2.6 and acid <= 5',
+    umami_x_sour: 'oaky <= 5 and acid >= 7.5',
+    umami_x_bitter: 'bitter <= 2.5 and oaky <= 2.5',
+    sweet_x_sour: 'sweet >= 5 and acid >= 7.5',
+    sweet_x_bitter: 'sweet >= 5 and bitter <= 2.5 and fruity >= 4 and acid >= 7.5',
+    sweet_x_spicy: 'sweet >= 5 and herby <= 2.5 and fruity >= 4.5 and acid >= 7.5',
+    sour_x_bitter: 'oaky <= 2.5 and bitter <= 2.5 and acid >= 5',
+    sour_x_spicy: 'sweet >= 5 and bitter <= 2.5 and alcohol <= 5',
+  }
 
 end
