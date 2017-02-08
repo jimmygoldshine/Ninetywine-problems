e8622e454b1a1301a8907f8f3a594f0ef2e88aaa
Mike Hurl <mikehurl@gmail.com>
2017-02-08 17:06:57 +0000

    deletes commented out code in files and refactors methods

--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -2,28 +2,20 @@ class Pairing < ApplicationRecord
 
   attr_accessor :wine
 
-  FIRST_FLAVOUR_RANGE = 0.75
-  SECOND_FLAVOUR_RANGE = 1.25
-  THIRD_FLAVOUR_RANGE = 2
-  FOURTH_FLAVOUR_RANGE = 3
-  FIFTH_FLAVOUR_RANGE = 4
+  FIRST_FLAVOUR_RANGE = 1
+  SECOND_FLAVOUR_RANGE = 2
+  THIRD_FLAVOUR_RANGE = 3
+  FOURTH_FLAVOUR_RANGE = 4
+  FIFTH_FLAVOUR_RANGE = 5
 
   has_one :food
 
   def get_wine(wine_klass)
-    if strongest_flavour == :sweet
-      @wine = wine_klass.where(sweet_query)
-    elsif strongest_flavour == :sour
-      @wine = wine_klass.where(sour_query)
-    elsif strongest_flavour == :bitter
-      @wine = wine_klass.where(bitter_query)
-    elsif strongest_flavour == :umami
-      @wine = wine_klass.where(umami_query)
-    elsif strongest_flavour == :spicy
-      @wine = wine_klass.where(spicy_query)
-    else
-      @wine = wine_klass.where(MATCHES[food.flavour]).limit(3)
-    end
+    @wine = wine_klass.where(sweet_query) if strongest_flavour == :sweet
+    @wine = wine_klass.where(sour_query) if strongest_flavour == :sour
+    @wine = wine_klass.where(bitter_query) if strongest_flavour == :bitter
+    @wine = wine_klass.where(umami_query) if strongest_flavour == :umami
+    @wine = wine_klass.where(spicy_query) if strongest_flavour == :spicy
   end
 
   def strongest_flavour
@@ -73,21 +65,5 @@ class Pairing < ApplicationRecord
     upper_sweet = (5 + food_flavours[:spicy].to_f * 0.2)
     "alcohol >= #{lower_alcohol} and alcohol <= #{upper_alcohol} and sweet >= #{lower_sweet} and sweet <= #{upper_sweet}"
   end
-
-  MATCHES = {
-    sweet: 'sweet >= 5',
-    spicy: 'sweet >= 2.6 and sweet <= 5 and fruity >= 5.1 and acid >= 5.1 and alcohol <= 5',
-    sour: 'sweet >= 5 and bitter <= 2.5 and oaky <= 2.5',
-    umami: 'sweet >= 5 and bitter <= 2.5 and fruity >= 5',
-    bitter: 'bitter <= 2.5 and oaky <= 2.5',
-    umami_x_sweet: 'fruity >= 5 and acid >= 2.6 and acid <= 5',
-    umami_x_sour: 'oaky <= 5 and acid >= 7.5',
-    umami_x_bitter: 'bitter <= 2.5 and oaky <= 2.5',
-    sweet_x_sour: 'sweet >= 5 and acid >= 7.5',
-    sweet_x_bitter: 'sweet >= 5 and bitter <= 2.5 and fruity >= 4 and acid >= 7.5',
-    sweet_x_spicy: 'sweet >= 5 and herby <= 2.5 and fruity >= 4.5 and acid >= 7.5',
-    sour_x_bitter: 'oaky <= 2.5 and bitter <= 2.5 and acid >= 5',
-    sour_x_spicy: 'sweet >= 5 and bitter <= 2.5 and alcohol <= 5',
-  }
 
 end
--- spec/pairing_spec.rb
+++ spec/pairing_spec.rb
@@ -3,24 +3,6 @@ require 'pairing'
 describe Pairing do
 
   let!(:wine_klass) { spy("Wine Klass") }
-  let!(:sweet_food) { double("Sweet Food", flavour: :sweet) }
-  let!(:spicy_food) { double("Spicy Food", flavour: :spicy) }
-  let!(:sour_food) { double("Sour Food", flavour: :sour) }
-  let!(:bitter_food) { double("Bitter Food", flavour: :bitter) }
-  let!(:umami_food) { double("Umami Food", flavour: :umami) }
-
-  let!(:umami_x_sweet_food) { double("Umami & Sweet Food", flavour: :umami_x_sweet) }
-  let!(:umami_x_sour_food) { double("Umami & Sour Food", flavour: :umami_x_sour) }
-  let!(:umami_x_bitter_food) { double("Umami & Bitter Food", flavour: :umami_x_bitter) }
-
-  let!(:sweet_x_sour_food) { double("Sweet & Sour Food", flavour: :sweet_x_sour) }
-  let!(:sweet_x_bitter_food) { double("Sweet & Bitter Food", flavour: :sweet_x_bitter) }
-  let!(:sweet_x_spicy_food) { double("Sweet & Spicy Food", flavour: :sweet_x_spicy) }
-
-  let!(:sour_x_bitter_food) { double("Sour & Bitter Food", flavour: :sour_x_bitter) }
-  let!(:sour_x_spicy_food) { double("Sour & Spicy Food", flavour: :sour_x_spicy) }
-
-  #doubles created on Wed 8th Fev below
   let!(:sweet_potato) { double('Sweet potato', flavour: {sweet: 4, umami: 2.5, sour: 0, bitter: 0, spicy: 0 }) }
   let!(:mushrooms) { double('Mushrooms', flavour: {sweet: 0.5, umami: 5, sour: 0.5, bitter: 2, spicy: 0 }) }
   let!(:ceviche) { double("Ceviche", flavour: {spicy: 3, umami: 3, sour: 4.5, bitter: 0.5, sweet: 0}) }
@@ -31,131 +13,38 @@ describe Pairing do
 
   describe "#get_wine" do
 
-    # it 'should get sweet wine from the db if the food is sweet' do
-    #   allow(pairing).to receive(:food).and_return(sweet_food)
-    #   pairing.get_wine(wine_klass)
-    #   criterion = 'sweet >= 5'
-    #   expect(wine_klass).to have_received(:where).with('sweet >= 5')
-    # end
-    #
-    # it 'should get spicy wine from the db if the food is spicy' do
-    #   allow(pairing).to receive(:food).and_return(spicy_food)
-    #   pairing.get_wine(wine_klass)
-    #   criteria = 'sweet >= 2.6 and sweet <= 5 and fruity >= 5.1 and acid >= 5.1 and alcohol <= 5'
-    #   expect(wine_klass).to have_received(:where).with(criteria)
-    # end
-    #
-    # it 'should get sour wine from the db if the food is sour' do
-    #   allow(pairing).to receive(:food).and_return(sour_food)
-    #   pairing.get_wine(wine_klass)
-    #   criteria = 'sweet >= 5 and bitter <= 2.5 and oaky <= 2.5'
-    #   expect(wine_klass).to have_received(:where).with(criteria)
-    # end
-    #
-    # it 'should get umami wine from the db if the food is umami' do
-    #   allow(pairing).to receive(:food).and_return(umami_food)
-    #   pairing.get_wine(wine_klass)
-    #   criteria = 'sweet >= 5 and bitter <= 2.5 and fruity >= 5'
-    #   expect(wine_klass).to have_received(:where).with(criteria)
-    # end
-    #
-    # it 'should get bitter wine from the db if the food is bitter' do
-    #   allow(pairing).to receive(:food).and_return(bitter_food)
-    #   pairing.get_wine(wine_klass)
-    #   criteria = 'bitter <= 2.5 and oaky <= 2.5'
-    #   expect(wine_klass).to have_received(:where).with(criteria)
-    # end
-    #
-    # it 'should be able to get wine appropriate for umami and sweet food' do
-    #   allow(pairing).to receive(:food).and_return(umami_x_sweet_food)
-    #   pairing.get_wine(wine_klass)
-    #   criteria = 'fruity >= 5 and acid >= 2.6 and acid <= 5'
-    #   expect(wine_klass).to have_received(:where).with(criteria)
-    # end
-    #
-    # it 'should be able to get wine appropriate for umami and sour food' do
-    #   allow(pairing).to receive(:food).and_return(umami_x_sour_food)
-    #   pairing.get_wine(wine_klass)
-    #   criteria = 'oaky <= 5 and acid >= 7.5'
-    #   expect(wine_klass).to have_received(:where).with(criteria)
-    # end
-    #
-    # it 'should be able to get wine appropriate for umami and bitter food' do
-    #   allow(pairing).to receive(:food).and_return(umami_x_bitter_food)
-    #   pairing.get_wine(wine_klass)
-    #   criteria = 'bitter <= 2.5 and oaky <= 2.5'
-    #   expect(wine_klass).to have_received(:where).with(criteria)
-    # end
-    #
-    # it 'should be able to get wine appropriate for sweet and sour food' do
-    #   allow(pairing).to receive(:food).and_return(sweet_x_sour_food)
-    #   pairing.get_wine(wine_klass)
-    #   criteria = 'sweet >= 5 and acid >= 7.5'
-    #   expect(wine_klass).to have_received(:where).with(criteria)
-    # end
-    #
-    # it 'should be able to get wine appropriate for sweet and bitter food' do
-    #   allow(pairing).to receive(:food).and_return(sweet_x_bitter_food)
-    #   pairing.get_wine(wine_klass)
-    #   criteria = 'sweet >= 5 and bitter <= 2.5 and fruity >= 4 and acid >= 7.5'
-    #   expect(wine_klass).to have_received(:where).with(criteria)
-    # end
-    #
-    # it 'should be able to get wine appropriate for sweet and spicy food' do
-    #   allow(pairing).to receive(:food).and_return(sweet_x_spicy_food)
-    #   pairing.get_wine(wine_klass)
-    #   criteria = 'sweet >= 5 and herby <= 2.5 and fruity >= 4.5 and acid >= 7.5'
-    #   expect(wine_klass).to have_received(:where).with(criteria)
-    # end
-    #
-    # it 'should be able to get wine appropriate for sour and bitter food' do
-    #   allow(pairing).to receive(:food).and_return(sour_x_bitter_food)
-    #   pairing.get_wine(wine_klass)
-    #   criteria = 'oaky <= 2.5 and bitter <= 2.5 and acid >= 5'
-    #   expect(wine_klass).to have_received(:where).with(criteria)
-    # end
-    #
-    # it 'should be able to get wine appropriate for sour and spicy food' do
-    #   allow(pairing).to receive(:food).and_return(sour_x_spicy_food)
-    #   pairing.get_wine(wine_klass)
-    #   criteria = 'sweet >= 5 and bitter <= 2.5 and alcohol <= 5'
-    #   expect(wine_klass).to have_received(:where).with(criteria)
-    # end
-
-
     it 'should return a narrow range of wine if the strongest value is sweet' do
       allow(pairing).to receive(:food).and_return(sweet_potato)
       pairing.get_wine(wine_klass)
-      criteria = 'sweet >= 7.5 and sweet <= 8.5'
+      criteria = 'sweet >= 7.0 and sweet <= 9.0'
       expect(wine_klass).to have_received(:where).with(criteria)
     end
 
     it 'should return a narrow range of wine if the strongest value is sour' do
       allow(pairing).to receive(:food).and_return(ceviche)
       pairing.get_wine(wine_klass)
-      criteria = 'acid >= 8.5 and acid <= 9.5'
+      criteria = 'acid >= 8.0 and acid <= 10.0'
       expect(wine_klass).to have_received(:where).with(criteria)
     end
 
     it 'should return a narrow range of wine if the strongest value is bitter' do
       allow(pairing).to receive(:food).and_return(rocket_salad)
       pairing.get_wine(wine_klass)
-      criteria = 'bitter >= 2.0 and bitter <= 3.0 and oaky >= 2.0 and oaky <= 3.0'
+      criteria = 'bitter >= 1.5 and bitter <= 3.5 and oaky >= 1.5 and oaky <= 3.5'
       expect(wine_klass).to have_received(:where).with(criteria)
     end
 
     it 'should return a narrow range of wine if the strongest value is umami' do
       allow(pairing).to receive(:food).and_return(mushrooms)
       pairing.get_wine(wine_klass)
-      criteria = 'fruity >= 9.5 and fruity <= 10.5 and oaky >= 7.0 and oaky <= 8.0'
+      criteria = 'fruity >= 9.0 and fruity <= 11.0 and oaky >= 6.5 and oaky <= 8.5'
       expect(wine_klass).to have_received(:where).with(criteria)
     end
 
     it 'should return a narrow range of wine if the strongest value is spicy' do
       allow(pairing).to receive(:food).and_return(phaal)
       pairing.get_wine(wine_klass)
-      criteria = 'alcohol >= 1.5 and alcohol <= 2.5 and sweet >= 4.0 and sweet <= 6.0'
-      #low alcohol, mid sweet
+      criteria = 'alcohol >= 1.0 and alcohol <= 3.0 and sweet >= 4.0 and sweet <= 6.0'
       expect(wine_klass).to have_received(:where).with(criteria)
     end
 
