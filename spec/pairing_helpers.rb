2260f3e069ed591692c3101bd4ee64afa1ccaa67
Mike Hurl <mikehurl@gmail.com>
2017-02-08 10:35:58 +0000

    creates #create_pairings(args) method as a helper for pairing features spec

--- spec/pairing_helpers.rb
+++ spec/pairing_helpers.rb
@@ -1,8 +1,19 @@
 module PairingHelpers
 
+  DEFAULT_FOOD_VALUE = 0
+
   def start_pairing
     visit '/'
     click_button('Start pairing!')
+  end
+
+  def create_pairing(args)
+    fill_in :food_name, with: args.fetch(:name, "A food")
+    fill_in :food_sweet, with: args.fetch(:sweet, DEFAULT_FOOD_VALUE)
+    fill_in :food_umami, with: args.fetch(:umami, DEFAULT_FOOD_VALUE)
+    fill_in :food_spicy, with: args.fetch(:spicy, DEFAULT_FOOD_VALUE)
+    fill_in :food_sour, with: args.fetch(:sour, DEFAULT_FOOD_VALUE)
+    fill_in :food_bitter, with: args.fetch(:bitter, DEFAULT_FOOD_VALUE)
   end
 
 end
