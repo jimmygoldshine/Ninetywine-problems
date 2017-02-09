b3e9f7a28ef65198219b703f9f1ef6d1f1d38ffa
Mike Hurl <mikehurl@gmail.com>
2017-02-09 11:20:47 +0000

    creates methods that return a query string taking into account the ranking of the food flavours

--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -2,11 +2,7 @@ class Pairing < ApplicationRecord
 
   attr_accessor :wine
 
-  FIRST_FLAVOUR_RANGE = 1
-  SECOND_FLAVOUR_RANGE = 2
-  THIRD_FLAVOUR_RANGE = 3
-  FOURTH_FLAVOUR_RANGE = 4
-  FIFTH_FLAVOUR_RANGE = 5
+  FLAVOUR_RANGE = [1, 2, 3, 4, 5]
 
   has_one :food
 
@@ -34,37 +30,54 @@ class Pairing < ApplicationRecord
     food_flavours.keep_if { |key, value| value >= 0.5 }
   end
 
+  def rank_flavours
+    eliminate_weak_flavours.sort_by { |key, value| value }.reverse.to_h
+  end
+
+  def query_array
+    index = 0
+    rank_flavours.collect do |flavour, strength|
+      query = method(flavour.to_s + "_query").call(index)
+      index += 1
+      query
+    end
+  end
+
+  def query_builder
+    query_array.join(' and ')
+  end
+
   private
 
-  def sweet_query
-    lower = (food_flavours[:sweet].to_f * 2) - FIRST_FLAVOUR_RANGE
-    upper = (food_flavours[:sweet].to_f * 2) + FIRST_FLAVOUR_RANGE
+  def sweet_query(index)
+    lower = (food_flavours[:sweet].to_f * 2) - FLAVOUR_RANGE[index]
+    upper = (food_flavours[:sweet].to_f * 2) + FLAVOUR_RANGE[index]
     "sweet >= #{lower} and sweet <= #{upper}"
   end
 
-  def sour_query
-    lower = (food_flavours[:sour].to_f * 2) - FIRST_FLAVOUR_RANGE
-    upper = (food_flavours[:sour].to_f * 2) + FIRST_FLAVOUR_RANGE
+  def sour_query(index)
+    lower = (food_flavours[:sour].to_f * 2) - FLAVOUR_RANGE[index]
+    upper = (food_flavours[:sour].to_f * 2) + FLAVOUR_RANGE[index]
     "acid >= #{lower} and acid <= #{upper}"
   end
 
-  def bitter_query
-    lower = ((1 / food_flavours[:bitter].to_f) * 10) - FIRST_FLAVOUR_RANGE
-    upper = ((1 / food_flavours[:bitter].to_f) * 10) + FIRST_FLAVOUR_RANGE
+  def bitter_query(index)
+    lower = ((1 / food_flavours[:bitter].to_f) * 10) - FLAVOUR_RANGE[index]
+    upper = ((1 / food_flavours[:bitter].to_f) * 10) + FLAVOUR_RANGE[index]
     "bitter >= #{lower} and bitter <= #{upper} and oaky >= #{lower} and oaky <= #{upper}"
   end
 
-  def umami_query
-    lower_fruity = (food_flavours[:umami].to_f * 2) - FIRST_FLAVOUR_RANGE
-    upper_fruity = (food_flavours[:umami].to_f * 2) + FIRST_FLAVOUR_RANGE
-    lower_oaky = (food_flavours[:umami].to_f * 1.5) - FIRST_FLAVOUR_RANGE
-    upper_oaky = (food_flavours[:umami].to_f * 1.5) + FIRST_FLAVOUR_RANGE
+  def umami_query(index)
+    lower_fruity = (food_flavours[:umami].to_f * 2) - FLAVOUR_RANGE[index]
+    upper_fruity = (food_flavours[:umami].to_f * 2) + FLAVOUR_RANGE[index]
+    lower_oaky = (food_flavours[:umami].to_f * 1.5) - FLAVOUR_RANGE[index]
+    upper_oaky = (food_flavours[:umami].to_f * 1.5) + FLAVOUR_RANGE[index]
     "fruity >= #{lower_fruity} and fruity <= #{upper_fruity} and oaky >= #{lower_oaky} and oaky <= #{upper_oaky}"
   end
 
-  def spicy_query
-    lower_alcohol = ((1 / food_flavours[:spicy].to_f) * 10) - FIRST_FLAVOUR_RANGE
-    upper_alcohol = ((1 / food_flavours[:spicy].to_f) * 10) + FIRST_FLAVOUR_RANGE
+  def spicy_query(index)
+    lower_alcohol = ((1 / food_flavours[:spicy].to_f) * 10) - FLAVOUR_RANGE[index]
+    upper_alcohol = ((1 / food_flavours[:spicy].to_f) * 10) + FLAVOUR_RANGE[index]
     lower_sweet = (5 - food_flavours[:spicy].to_f * 0.2)
     upper_sweet = (5 + food_flavours[:spicy].to_f * 0.2)
     "alcohol >= #{lower_alcohol} and alcohol <= #{upper_alcohol} and sweet >= #{lower_sweet} and sweet <= #{upper_sweet}"
--- spec/pairing_spec.rb
+++ spec/pairing_spec.rb
@@ -3,7 +3,7 @@ require 'pairing'
 describe Pairing do
 
   let!(:wine_klass) { spy("Wine Klass") }
-  let!(:sweet_potato) { double('Sweet potato', flavour: {sweet: 4, umami: 2.5, sour: 0, bitter: 0, spicy: 0 }) }
+  let!(:sweet_potato) { double('Sweet potato', flavour: {umami: 2.5, sweet: 4, sour: 0, bitter: 0, spicy: 0 }) }
   let!(:mushrooms) { double('Mushrooms', flavour: {sweet: 0.5, umami: 5, sour: 0.5, bitter: 2, spicy: 0 }) }
   let!(:ceviche) { double("Ceviche", flavour: {spicy: 3, umami: 3, sour: 4.5, bitter: 0.5, sweet: 0}) }
   let!(:rocket_salad) { double('Rocket Salad', flavour: {spicy: 0, umami: 2, sour: 1, bitter: 4, sweet: 1}) }
@@ -96,6 +96,28 @@ describe Pairing do
       expect(pairing.eliminate_weak_flavours).to eq({sweet:0.5, umami:5, sour:0.5, bitter:2})
     end
 
+  end
+
+  describe '#rank_flavours' do
+
+    it 'ranks the food characteristics from highest to lowest after #eliminate_weak_flavours has been called' do
+      allow(pairing).to receive(:food).and_return(sweet_potato)
+      expect(pairing.rank_flavours.first).to eq [:sweet, 4]
+    end
+  end
+
+  describe '#query_array' do
+    it 'returns an array of queries based on food flavours' do
+      allow(pairing).to receive(:food).and_return(sweet_potato)
+      expect(pairing.query_array).to eq ["sweet >= 7.0 and sweet <= 9.0", "fruity >= 3.0 and fruity <= 7.0 and oaky >= 1.75 and oaky <= 5.75"]
+    end
+  end
+
+  describe '#query_builder' do
+    it 'returns a string containing the full query needed to return wines from the database' do
+      allow(pairing).to receive(:food).and_return(sweet_potato)
+      expect(pairing.query_builder).to eq 'sweet >= 7.0 and sweet <= 9.0 and fruity >= 3.0 and fruity <= 7.0 and oaky >= 1.75 and oaky <= 5.75'
+    end
   end
 
 end
