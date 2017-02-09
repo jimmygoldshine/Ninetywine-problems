37282cb18ca9927113608d2be73184079df13f76
James <jamesdix@cwgsy.net>
2017-02-09 12:08:27 +0000

    jquery completed for bar charts

---
+++ app/assets/javascripts/charts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $("#chart-id-1").hide();
+  $("#chart-id-2").hide();
+  $("#chart-id-3").hide();
+
+  $("#wine-id-1").click(function(){
+    $("#chart-id-2").hide();
+    $("#chart-id-3").hide();
+    $("#chart-id-1").show( 1000 );
+  });
+
+  $("#wine-id-2").click(function(){
+    $("#chart-id-1").hide()
+    $("#chart-id-3").hide();
+    $("#chart-id-2").show(1000);
+  });
+
+  $("#wine-id-3").click(function(){
+    $("#chart-id-1").hide()
+    $("#chart-id-2").hide();
+    $("#chart-id-3").show(1000);
+  });
+});
